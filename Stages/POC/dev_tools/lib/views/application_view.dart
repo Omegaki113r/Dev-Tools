cab54e183f5a0d9816126d07c61a03dd49f6c566
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-01-08 23:16:29 +0530

    moved MaterialApp

--- Stages/POC/dev_tools/lib/views/application_view.dart
+++ Stages/POC/dev_tools/lib/views/application_view.dart
@@ -1,6 +1,8 @@
 import 'package:dev_tools/const/app_colors.dart';
+import 'package:dev_tools/const/app_themes.dart';
 import 'package:dev_tools/providers/app_provider.dart';
 import 'package:dev_tools/views/type_converter_view.dart';
+import 'package:dev_tools/widgets/side_bar_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
@@ -10,64 +12,47 @@ class ApplicationView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: color6MaterialColor,
-          accentColor: color6MaterialColor,
-        ),
-      ),
-      darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: color6MaterialColor,
-          accentColor: color6MaterialColor,
-        ),
-      ),
-      home: Scaffold(
-        body: Row(
-          children: [
-            Expanded(
-                flex: 1,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                SidebarButton(
-                          "Type\nConverter",
-                  onPressed: () {
-                    print("pressed");
-                  },
-                        ),
+    return Scaffold(
+      body: Row(
+        children: [
+          SizedBox(
+            width: 200,
+            child: Column(
+              children: [
                 SidebarButton(
                   "Type\nConverter",
-                  onPressed: () {
-                    print("pressed");
-                  },
-                      ),
-                SidebarButton(
-                  "Type\nConverter",
-                  onPressed: () {
-                    print("pressed");
-                  },
-                    ),
-                SidebarButton(
-                        "Type\nConverter",
                   onPressed: () {
                     print("pressed");
                   },
                 ),
-                  ],
+                SidebarButton(
+                  "Type\nConverter",
+                  onPressed: () {
+                    print("pressed");
+                  },
+                ),
+                SidebarButton(
+                  "Type\nConverter",
+                  onPressed: () {
+                    print("pressed");
+                  },
+                ),
+                SidebarButton(
+                  "Type\nConverter",
+                  onPressed: () {
+                    print("pressed");
+                  },
+                ),
+              ],
             ),
           ),
-            Expanded(
-              flex: 10,
-              child: switch (context.watch<AppProvider>().selectedView) {
-                SelectedView.TYPE_CONVERTER => const TypeConverterView(),
-                SelectedView.STREAMER => Container()
-              },
-            )
-          ],
-        ),
+          Expanded(
+            child: switch (context.watch<AppProvider>().selectedView) {
+              SelectedView.TYPE_CONVERTER => const TypeConverterView(),
+              SelectedView.STREAMER => Container()
+            },
+          )
+        ],
       ),
     );
   }
