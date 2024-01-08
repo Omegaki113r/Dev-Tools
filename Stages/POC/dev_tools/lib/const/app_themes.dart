6e2ff7a330e6853a0ae2591f450e976a1f9cc451
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-01-08 23:13:59 +0530

    added style for text button for sidebar

--- Stages/POC/dev_tools/lib/const/app_themes.dart
+++ Stages/POC/dev_tools/lib/const/app_themes.dart
@@ -4,3 +4,18 @@
       fontWeight: FontWeight.normal,
     ),
   ),
+  textButtonTheme: TextButtonThemeData(
+    style: ButtonStyle(
+      overlayColor:
+          MaterialStateProperty.resolveWith<Color>((Set<MaterialState> states) {
+        if (states.contains(MaterialState.focused)) return Colors.red;
+        if (states.contains(MaterialState.hovered)) return Colors.transparent;
+        if (states.contains(MaterialState.pressed)) return Colors.blue;
+        return Colors.grey; // Defer to the widget's default.
+      }),
+      // minimumSize: const MaterialStatePropertyAll(Size.zero),
+      padding: const MaterialStatePropertyAll(
+        EdgeInsets.all(0),
+      ),
+    ),
+  ),
