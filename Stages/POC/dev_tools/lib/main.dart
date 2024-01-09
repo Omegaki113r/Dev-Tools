820327dc8e86dfb2b81ae7c547c617557235e7f4
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-01-10 04:01:00 +0530

    adjusting dependency according to alphebetical order

--- Stages/POC/dev_tools/lib/main.dart
+++ Stages/POC/dev_tools/lib/main.dart
@@ -1,17 +1,15 @@
 import 'dart:io';
 
-import 'package:dev_tools/const/app_colors.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+import 'package:window_manager/window_manager.dart';
+
 import 'package:dev_tools/const/app_themes.dart';
-import 'package:dev_tools/models/type_converter_model.dart';
 import 'package:dev_tools/providers/app_provider.dart';
 import 'package:dev_tools/providers/type_converter_provider.dart';
 import 'package:dev_tools/views/application_view.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-// import 'package:provider/provider.dart';
-import 'package:window_manager/window_manager.dart';
-import 'package:desktop_window/desktop_window.dart';
 
 void main() async {
   if (kIsWeb) {
