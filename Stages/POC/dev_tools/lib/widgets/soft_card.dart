b417dc09606b5e80473b9debec81ccd7914e8e96
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-01-15 11:54:16 +0530

    soft card added

---
+++ Stages/POC/dev_tools/lib/widgets/soft_card.dart
@@ -0,0 +1,38 @@
+import 'package:dev_tools/const/app_colors.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class SoftCard extends StatelessWidget {
+  final Widget child;
+  final double? height;
+  final double? width;
+  const SoftCard({super.key, required this.child, this.height, this.width});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color(0xFF2C2754),
+          Color(0xFF0F0B2F),
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(-10, -10),
+            blurRadius: 20,
+            color: Color(0xFF312C5E),
+          ),
+          BoxShadow(
+            offset: Offset(10, 10),
+            blurRadius: 20,
+            color: Color(0xFF050227),
+          ),
+        ],
+      ),
+      child: child,
+    );
+  }
+}
