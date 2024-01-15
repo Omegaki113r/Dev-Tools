aa51e913fec776551b855d5e6dec979ddea970f2
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-01-15 11:55:09 +0530

    soft text added

---
+++ Stages/POC/dev_tools/lib/widgets/soft_text.dart
@@ -0,0 +1,55 @@
+import 'package:dev_tools/const/app_colors.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
+import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
+import 'package:gap/gap.dart';
+
+class SoftText extends StatelessWidget {
+  final String? label;
+  final String text;
+  final double? height;
+  final double? width;
+  const SoftText(this.text, {super.key, this.label, this.height, this.width});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: const BoxDecoration(
+        color: color6,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(-10, -10),
+            blurRadius: 20,
+            spreadRadius: -10,
+            color: Color(0xFF312C5E),
+            inset: true,
+          ),
+          BoxShadow(
+            offset: Offset(10, 10),
+            blurRadius: 20,
+            spreadRadius: -10,
+            color: Color(0xFF050227),
+            inset: true,
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(label ?? ""),
+            Gap(10),
+            Text(
+              text,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
