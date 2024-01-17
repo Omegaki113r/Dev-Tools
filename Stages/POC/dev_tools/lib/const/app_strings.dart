36e85c62e66f24e86a9935680a66316d5b4dbb78
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-01-17 15:49:07 +0530

    Strings added

---
+++ Stages/POC/dev_tools/lib/const/app_strings.dart
@@ -0,0 +1,13 @@
+const String APP_NAME = "Xtronic DevTools";
+
+const String LBL_CONNECT = "Connect";
+const String LBL_DISCONNECT = "Disconnect";
+
+const String LBL_BITWISE_CONVERTER = "Bitwise Calculator";
+const String LBL_DECIMAL = "Decimal";
+const String LBL_2S_COMPLIMENT = "Decimal 2's Compliment";
+const String LBL_BINARY = "Binary";
+const String LBL_OCTAL = "Octal";
+const String LBL_HEX = "Hex";
+
+const String LBL_DATA_STREAMER = "Data Streamer";
