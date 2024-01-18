a9d1f85b346a05d7671e8bd6c63ef64783f9e1ac
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-01-18 19:09:09 +0530

    titlebox Text implemented

--- Stages/POC/dev_tools/lib/widgets/soft_text.dart
+++ Stages/POC/dev_tools/lib/widgets/soft_text.dart
@@ -9,51 +9,91 @@ class SoftText extends StatelessWidget {
   final String text;
   final double? height;
   final double? width;
-  const SoftText(this.text, {super.key, this.label, this.height, this.width});
+  bool isTitleBox = false;
+  SoftText(this.text,
+      {super.key,
+      this.label,
+      this.height,
+      this.width,
+      this.isTitleBox = false});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: height,
-      decoration: const BoxDecoration(
-        color: color6,
-        borderRadius: BorderRadius.all(Radius.circular(20)),
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(-10, -10),
-            blurRadius: 20,
-            spreadRadius: -10,
-            color: Color(0xFF312C5E),
-            inset: true,
-          ),
-          BoxShadow(
-            offset: Offset(10, 10),
-            blurRadius: 20,
-            spreadRadius: -10,
-            color: Color(0xFF050227),
-            inset: true,
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              label ?? "",
-              maxLines: 1,
+    if (isTitleBox) {
+      return Container(
+        width: width,
+        height: height,
+        decoration: const BoxDecoration(
+          color: color6,
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(-10, -10),
+              blurRadius: 20,
+              spreadRadius: -10,
+              color: Color(0xFF312C5E),
+              inset: true,
             ),
-            Gap(10),
-            Text(
-              text,
-              style: Theme.of(context).textTheme.bodyLarge,
-              maxLines: 1,
+            BoxShadow(
+              offset: Offset(10, 10),
+              blurRadius: 20,
+              spreadRadius: -10,
+              color: Color(0xFF050227),
+              inset: true,
             ),
           ],
         ),
-      ),
-    );
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: Text(
+            text,
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+        ),
+      );
+    } else {
+      return Container(
+        width: width,
+        height: height,
+        decoration: const BoxDecoration(
+          color: color6,
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(-10, -10),
+              blurRadius: 20,
+              spreadRadius: -10,
+              color: Color(0xFF312C5E),
+              inset: true,
+            ),
+            BoxShadow(
+              offset: Offset(10, 10),
+              blurRadius: 20,
+              spreadRadius: -10,
+              color: Color(0xFF050227),
+              inset: true,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                label ?? "",
+                maxLines: 1,
+              ),
+              Gap(10),
+              Text(
+                text,
+                style: Theme.of(context).textTheme.bodyLarge,
+                maxLines: 1,
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
