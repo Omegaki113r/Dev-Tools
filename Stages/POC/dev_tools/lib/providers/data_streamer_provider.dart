059d5a5978ae3bffdc437c7298b617b9c501f8f2
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-01-20 16:07:04 +0530

    fixed the error where disconnection is not working due to buffer refresh on dropdown rebuild

--- Stages/POC/dev_tools/lib/providers/data_streamer_provider.dart
+++ Stages/POC/dev_tools/lib/providers/data_streamer_provider.dart
@@ -7,64 +7,77 @@ import 'package:flutter_libserialport/flutter_libserialport.dart';
 import 'package:logger/logger.dart';
 
 class DataStreamerProvider with ChangeNotifier {
-  Logger _logger = Logger(printer: SimplePrinter());
-  DeepCollectionEquality _equalityChecker = DeepCollectionEquality();
+  final Logger _logger = Logger(printer: SimplePrinter());
   Map<String, SerialPort?> portList = {};
   SerialPort? _selectedPort;
-
+  SerialPortReader? _serialPortReader;
+  Stream? _incomingDataStream;
+  String serialData = "";
   DataStreamerProvider() {
     Timer.periodic(Duration(seconds: 1), (timer) {
-      List<String> _portNameList = SerialPort.availablePorts;
-      Map<String, SerialPort?> _portMap = {};
-      for (String portName in _portNameList) {
-        _portMap.addAll({portName: null});
-      }
-      if (!_equalityChecker.equals(portList, _portMap)) {
-        portList = _portMap;
-        notifyListeners();
+      List<String> portNameList = SerialPort.availablePorts;
+      for (String portName in portNameList) {
+        if (!portList.keys.contains(portName)) {
+          portList[portName] = null;
+          notifyListeners();
+        }
       }
     });
   }
 
-  void selectedSerialPortChanged(value) {
-    print(value);
-    // if (_selectedPort == null) {
-    //   _selectedPort = SerialPort(value);
-    //   SerialPortConfig config = _selectedPort!.config;
-    //   config.baudRate = 115200;
-    //   config.bits = 8;
-    //   config.parity = 0;
-    //   config.stopBits = 1;
-    //   _selectedPort!.config = config;
-    //   bool openState = _selectedPort!.open(mode: SerialPortMode.readWrite);
-    //   if (!openState) {
-    //     bool closeState = _selectedPort!.close();
-    //     openState = _selectedPort!.open(mode: SerialPortMode.readWrite);
-    //   }
-    //   SerialPortReader serialPortReader = SerialPortReader(_selectedPort!);
-    //   Stream incomingDataStream = serialPortReader.stream.asyncMap((data) {
-    //     return String.fromCharCodes(data);
-    //   });
-    //   incomingDataStream.listen((data) {
-    //     // print(data);
-    //     // _logger.i(data);
-    //   });
-    // } else if (_selectedPort != null && !_selectedPort!.isOpen) {
-    //   _selectedPort = SerialPort(value);
-    //   SerialPortConfig config = _selectedPort!.config;
-    //   config.baudRate = 115200;
-    //   config.bits = 8;
-    //   config.parity = 0;
-    //   config.stopBits = 1;
-    //   _selectedPort!.config = config;
-    //   _selectedPort!.open(mode: SerialPortMode.readWrite);
-    //   SerialPortReader serialPortReader = SerialPortReader(_selectedPort!);
-    //   Stream incomingDataStream = serialPortReader.stream.asyncMap((data) {
-    //     return String.fromCharCodes(data);
-    //   });
-    //   incomingDataStream.listen((data) {
-    //     print(data);
-    //   });
-    // }
+  void serialPortConnect() {
+    if (_selectedPort != null && !_selectedPort!.isOpen) {
+      SerialPortConfig config = _selectedPort!.config;
+      config.baudRate = 115200;
+      config.bits = 8;
+      config.parity = 0;
+      config.stopBits = 1;
+      _selectedPort!.config = config;
+      bool openState = _selectedPort!.openReadWrite();
+      if (openState) {
+        try {
+          _serialPortReader = SerialPortReader(_selectedPort!);
+          _incomingDataStream = _serialPortReader!.stream.asyncMap((data) {
+            return String.fromCharCodes(data);
+          });
+          _incomingDataStream!.listen((data) {
+            serialData += data;
+            // print(data);
+            // _logger.i(data);
+            notifyListeners();
+          });
+          notifyListeners();
+        } on SerialPortError catch (err, _) {
+          print(SerialPort.lastError);
+          _selectedPort!.close();
+        }
+      } else {
+        _serialPortReader!.close();
+        bool closeState = _selectedPort!.close();
+      }
+    }
   }
+
+  void serialPortDisconnect() {
+    if (_selectedPort != null && _selectedPort!.isOpen) {
+      _serialPortReader!.close();
+      bool closeState = _selectedPort!.close();
+      if (closeState) {
+        notifyListeners();
+      }
+    }
+  }
+
+  void selectedSerialPortChanged(value) {
+    if (_selectedPort == null) {
+      _selectedPort = SerialPort(value);
+    } else if (value != _selectedPort!.name) {
+      _selectedPort = SerialPort(value);
+    }
+    if (_selectedPort != null) {
+      portList.update(value, (value) => _selectedPort);
+    }
+  }
+
+  SerialPort? get port => _selectedPort;
 }
