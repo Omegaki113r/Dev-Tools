5ff8344cda6181dda552068cb83cde04d0b4e59f
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-02-09 23:47:34 +0530

    introduced flat dropdown button

--- Stages/POC/dev_tools/lib/widgets/soft_dropdown_button.dart
+++ Stages/POC/dev_tools/lib/widgets/soft_dropdown_button.dart
@@ -14,6 +14,8 @@ class SoftDropDownButton<T> extends StatelessWidget {
   final double? height;
   final T? selectedValue;
   final Function? onChanged;
+  final bool isFlat;
+
   const SoftDropDownButton(
     this.noItemText,
     this.label, {
@@ -26,28 +28,48 @@ class SoftDropDownButton<T> extends StatelessWidget {
     this.noItemTextStyle,
     this.labelTextStyle,
     this.itemStyle,
-  });
+  }) : isFlat = false;
+
+  const SoftDropDownButton.flat(
+    this.noItemText,
+    this.label, {
+    super.key,
+    this.height,
+    this.width,
+    this.selectedValue,
+    required this.itemList,
+    this.onChanged,
+    this.noItemTextStyle,
+    this.labelTextStyle,
+    this.itemStyle,
+  }) : isFlat = true;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: width,
       height: height,
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
+        // color: Color(0xFF1D1A39),
         gradient: LinearGradient(colors: [
-          Color(0xFF0F0B2F),
-          Color(0xFF2C2754),
+          if (isFlat) ...[
+            const Color(0xFF1D1A39),
+            const Color(0xFF1D1A39)
+          ] else ...[
+            const Color(0xFF0F0B2F),
+            const Color(0xFF2C2754),
+          ]
         ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-        borderRadius: BorderRadius.all(Radius.circular(20)),
-        boxShadow: [
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+        boxShadow: const [
           BoxShadow(
-            offset: Offset(-10, -10),
-            blurRadius: 20,
+            offset: Offset(-5, -5),
+            blurRadius: 10,
             color: Color(0xFF312C5E),
           ),
           BoxShadow(
-            offset: Offset(10, 10),
-            blurRadius: 20,
+            offset: Offset(5, 5),
+            blurRadius: 10,
             color: Color(0xFF050227),
           ),
         ],
@@ -70,7 +92,6 @@ class SoftDropDownButton<T> extends StatelessWidget {
                       iconEnabledColor: color2,
                       value: selectedValue,
                       isExpanded: true,
-                      isDense: true,
                       underline: Container(),
                       items: itemList,
                       style: itemStyle,
