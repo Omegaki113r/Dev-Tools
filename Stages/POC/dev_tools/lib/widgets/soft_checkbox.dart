491ac8dab5fdf87c65f154e68cfdaa8f803d5f25
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-02-09 23:47:44 +0530

    refactoring

--- Stages/POC/dev_tools/lib/widgets/soft_checkbox.dart
+++ Stages/POC/dev_tools/lib/widgets/soft_checkbox.dart
@@ -1,51 +1,46 @@
+import 'package:dev_tools/const/app_colors.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
 class SoftCheckbox extends StatelessWidget {
   final String label;
-  final Function(bool?)? onChanged;
+  final TextStyle? labelStyle;
+  final double? iconSize;
+  final Function(bool?) onChanged;
   final bool value;
-  const SoftCheckbox(this.label,
-      {super.key, required this.onChanged, required this.value});
+  const SoftCheckbox(
+    this.label, {
+    super.key,
+    required this.onChanged,
+    required this.value,
+    this.labelStyle,
+    this.iconSize,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-      child: GestureDetector(
-        onTap: () => onChanged!(!value),
-        child: AnimatedSwitcher(
-          duration: const Duration(milliseconds: 500),
-          child: value
-              ? Row(
-                  key: ValueKey(1),
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    // Checkbox(
-                    //   value: value,
-                    //   onChanged: onChanged,
-                    // ),
-                    Icon(Icons.check_sharp),
-                    const Gap(10),
-                    Text(label),
-                  ],
-                )
-              : Row(
-                  key: ValueKey(2),
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    // Checkbox(
-                    //   value: value,
-                    //   onChanged: onChanged,
-                    // ),
-                    Icon(Icons.check_box_outline_blank),
-                    const Gap(10),
-                    Text(label),
-                  ],
-                ),
-        ),
+      // child: GestureDetector(
+      //   onTap: () => onChanged!(!value),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Checkbox(
+            value: value,
+            onChanged: onChanged,
+            splashRadius: 0,
+            side: BorderSide(color: Color(0xFFFFFFFF).withOpacity(0.08)),
+          ),
+          const Gap(10),
+          Text(
+            label,
+            style: labelStyle,
+          ),
+        ],
       ),
+      // ),
     );
   }
 }
