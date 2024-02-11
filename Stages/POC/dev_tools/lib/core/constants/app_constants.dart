4a7149a0498985bcd2ec86ddcc2fe1e44ae2f66b
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-02-12 01:42:21 +0530

    enums moved out

--- Stages/POC/dev_tools/lib/core/constants/app_constants.dart
+++ Stages/POC/dev_tools/lib/core/constants/app_constants.dart
@@ -3,3 +3,7 @@ const bitwiseCalculatorRoute = "/TYPE_CONVERTER";
 const dataStreamerRoute = "/DATA_STREAMER";
 
 enum ButtonType { flat, concave, convex, emboss }
+
+enum SelectedView { bitwiseCalculator, streamer }
+
+enum StreamerType { serial, websocket, mqtt }
