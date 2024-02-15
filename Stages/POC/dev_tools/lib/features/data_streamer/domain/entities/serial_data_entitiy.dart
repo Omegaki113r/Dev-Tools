8eab967f8e6ea630e3342dc8ee8fdeca4275467e
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-02-16 00:54:57 +0530

    created serial data entity

---
+++ Stages/POC/dev_tools/lib/features/data_streamer/domain/entities/serial_data_entitiy.dart
@@ -0,0 +1,8 @@
+class SerialDataEntity {
+  final String ascii;
+  final String binary;
+  final String decimal;
+  final String hex;
+
+  const SerialDataEntity(this.ascii, this.binary, this.decimal, this.hex);
+}
