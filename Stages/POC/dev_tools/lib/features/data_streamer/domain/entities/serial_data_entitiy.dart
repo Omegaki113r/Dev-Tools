b0520bf92a995c8b977f0c7fb1bd345316d309ef
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-02-21 19:58:53 +0530

    renamed entity file

--- Stages/POC/dev_tools/lib/features/data_streamer/domain/entities/serial_data_entitiy.dart
+++
@@ -1,8 +0,0 @@
-class SerialDataEntity {
-  final String ascii;
-  final String binary;
-  final String decimal;
-  final String hex;
-
-  const SerialDataEntity(this.ascii, this.binary, this.decimal, this.hex);
-}
---
+++ Stages/POC/dev_tools/lib/features/data_streamer/domain/entities/stream_data_entitiy.dart
@@ -0,0 +1,8 @@
+class StreamDataEntity {
+  final String ascii;
+  final String binary;
+  final String decimal;
+  final String hex;
+
+  const StreamDataEntity(this.ascii, this.binary, this.decimal, this.hex);
+}
