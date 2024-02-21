ee22911e6905abf43784690ea65bc99cd44d92d5
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-02-21 20:02:11 +0530

    StreamDataCell

---
+++ Stages/POC/dev_tools/lib/features/data_streamer/presentation/widgets/stream_data_cell.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class StreamDataCell extends StatelessWidget {
+  final String? ascii;
+  final String? binary;
+  final String? decimal;
+  final String? hex;
+  const StreamDataCell(
+      {super.key, this.ascii, this.binary, this.decimal, this.hex});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 100,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            ascii ?? "",
+            style: const TextStyle(
+              color: Colors.red,
+            ),
+            textAlign: TextAlign.end,
+          ),
+          Text(
+            binary ?? "",
+            style: const TextStyle(
+              color: Colors.yellow,
+            ),
+          ),
+          Text(
+            decimal ?? "",
+            style: const TextStyle(
+              color: Colors.blue,
+            ),
+          ),
+          Text(
+            hex ?? "",
+            style: const TextStyle(
+              color: Colors.amber,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
