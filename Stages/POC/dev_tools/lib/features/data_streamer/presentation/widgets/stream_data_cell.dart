6f7276ff9861ae2eb40dd2313ac4fc5e7bcbfba8
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-02-23 22:02:28 +0530

    implemented pretty datacell from figma

--- Stages/POC/dev_tools/lib/features/data_streamer/presentation/widgets/stream_data_cell.dart
+++ Stages/POC/dev_tools/lib/features/data_streamer/presentation/widgets/stream_data_cell.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/material.dart';
+import 'package:dev_tools/core/constants/app_colors.dart';
+import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
+import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
+import 'package:gap/gap.dart';
 
 class StreamDataCell extends StatelessWidget {
   final String? ascii;
@@ -12,36 +15,158 @@ class StreamDataCell extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: 100,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            ascii ?? "",
-            style: const TextStyle(
-              color: Colors.red,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+            color6,
+            color6,
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(-5, -5),
+              blurRadius: 10,
+              color: Color(0xFF312C5E),
             ),
-            textAlign: TextAlign.end,
-          ),
-          Text(
-            hex ?? "",
-            style: const TextStyle(
-              color: Colors.amber,
+            BoxShadow(
+              offset: Offset(5, 5),
+              blurRadius: 10,
+              color: Color(0xFF050227),
             ),
-          ),
-          Text(
-            decimal ?? "",
-            style: const TextStyle(
-              color: Colors.blue,
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Gap(4),
+            Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: color6,
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(-10, -10),
+                    blurRadius: 20,
+                    spreadRadius: -10,
+                    color: Color(0xFF312C5E),
+                    inset: true,
+                  ),
+                  BoxShadow(
+                    offset: Offset(10, 10),
+                    blurRadius: 20,
+                    spreadRadius: -10,
+                    color: Color(0xFF050227),
+                    inset: true,
+                  ),
+                ],
+              ),
+              child: Text(
+                ascii ?? "",
+                style: const TextStyle(
+                  color: Colors.red,
+                ),
+                textAlign: TextAlign.end,
+              ),
             ),
-          ),
-          Text(
-            binary ?? "",
-            style: const TextStyle(
-              color: Colors.yellow,
+            const Gap(4),
+            Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: color6,
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(-10, -10),
+                    blurRadius: 20,
+                    spreadRadius: -10,
+                    color: Color(0xFF312C5E),
+                    inset: true,
+                  ),
+                  BoxShadow(
+                    offset: Offset(10, 10),
+                    blurRadius: 20,
+                    spreadRadius: -10,
+                    color: Color(0xFF050227),
+                    inset: true,
+                  ),
+                ],
+              ),
+              child: Text(
+                hex ?? "",
+                style: const TextStyle(
+                  color: Colors.amber,
+                ),
+                textAlign: TextAlign.end,
+              ),
             ),
-          ),
-        ],
+            const Gap(4),
+            Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: color6,
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(-10, -10),
+                    blurRadius: 20,
+                    spreadRadius: -10,
+                    color: Color(0xFF312C5E),
+                    inset: true,
+                  ),
+                  BoxShadow(
+                    offset: Offset(10, 10),
+                    blurRadius: 20,
+                    spreadRadius: -10,
+                    color: Color(0xFF050227),
+                    inset: true,
+                  ),
+                ],
+              ),
+              child: Text(
+                decimal ?? "",
+                style: const TextStyle(
+                  color: Colors.blue,
+                ),
+                textAlign: TextAlign.end,
+              ),
+            ),
+            const Gap(4),
+            Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: color6,
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(-10, -10),
+                    blurRadius: 20,
+                    spreadRadius: -10,
+                    color: Color(0xFF312C5E),
+                    inset: true,
+                  ),
+                  BoxShadow(
+                    offset: Offset(10, 10),
+                    blurRadius: 20,
+                    spreadRadius: -10,
+                    color: Color(0xFF050227),
+                    inset: true,
+                  ),
+                ],
+              ),
+              child: Text(
+                binary ?? "",
+                style: const TextStyle(
+                  color: Colors.yellow,
+                ),
+                textAlign: TextAlign.end,
+              ),
+            ),
+            const Gap(4),
+          ],
+        ),
       ),
     );
   }
