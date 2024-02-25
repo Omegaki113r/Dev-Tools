733b320e35b43c12b5e6fe9ae102acb9911b7483
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-02-25 13:30:39 +0530

    flat and elevated version

--- Stages/POC/dev_tools/lib/features/data_streamer/presentation/widgets/stream_data_cell.dart
+++ Stages/POC/dev_tools/lib/features/data_streamer/presentation/widgets/stream_data_cell.dart
@@ -8,101 +8,180 @@ class StreamDataCell extends StatelessWidget {
   final String? binary;
   final String? decimal;
   final String? hex;
+  final bool isFlat;
   const StreamDataCell(
-      {super.key, this.ascii, this.binary, this.decimal, this.hex});
+      {super.key, this.ascii, this.binary, this.decimal, this.hex})
+      : isFlat = false;
+
+  const StreamDataCell.Flat(
+      {super.key, this.ascii, this.binary, this.decimal, this.hex})
+      : isFlat = true;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
-          color6,
-          color6,
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(-5, -5),
-            blurRadius: 10,
-            color: Color(0xFF312C5E),
-          ),
-          BoxShadow(
-            offset: Offset(5, 5),
-            blurRadius: 10,
-            color: Color(0xFF050227),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          const Gap(4),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-                ascii ?? "",
-                style: const TextStyle(
-                  color: Colors.red,
-                  fontSize: 12.0,
+    return isFlat
+        ? Container(
+            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Gap(4),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      ascii ?? "",
+                      style: const TextStyle(
+                        color: Colors.red,
+                        fontSize: 12.0,
+                      ),
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
                 ),
-                textAlign: TextAlign.end,
-              ),
-            ),
-          ),
-          const Divider(
-            indent: 30,
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-                hex ?? "",
-                style: const TextStyle(
-                  color: Colors.amber,
-                  fontSize: 12.0,
+                const Divider(
+                  indent: 30,
                 ),
-                textAlign: TextAlign.end,
-              ),
-            ),
-          ),
-          const Divider(
-            indent: 30,
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-                decimal ?? "",
-                style: const TextStyle(
-                  color: Colors.blue,
-                  fontSize: 12.0,
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      hex ?? "",
+                      style: const TextStyle(
+                        color: Colors.amber,
+                        fontSize: 12.0,
+                      ),
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
                 ),
-                textAlign: TextAlign.end,
-              ),
-            ),
-          ),
-          const Divider(
-            indent: 30,
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-                binary ?? "",
-                style: const TextStyle(
-                  color: Colors.yellow,
-                  fontSize: 12.0,
+                const Divider(
+                  indent: 30,
                 ),
-                textAlign: TextAlign.end,
-              ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      decimal ?? "",
+                      style: const TextStyle(
+                        color: Colors.blue,
+                        fontSize: 12.0,
+                      ),
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
+                ),
+                const Divider(
+                  indent: 30,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      binary ?? "",
+                      style: const TextStyle(
+                        color: Colors.yellow,
+                        fontSize: 12.0,
+                      ),
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
+                ),
+                const Gap(4),
+              ],
             ),
-          ),
-          const Gap(4),
-        ],
-      ),
-    );
+          )
+        : Container(
+            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+                color6,
+                color6,
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(-5, -5),
+                  blurRadius: 10,
+                  color: Color(0xFF312C5E),
+                ),
+                BoxShadow(
+                  offset: Offset(5, 5),
+                  blurRadius: 10,
+                  color: Color(0xFF050227),
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const Gap(4),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      ascii ?? "",
+                      style: const TextStyle(
+                        color: Colors.red,
+                        fontSize: 12.0,
+                      ),
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
+                ),
+                const Divider(
+                  indent: 30,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      hex ?? "",
+                      style: const TextStyle(
+                        color: Colors.amber,
+                        fontSize: 12.0,
+                      ),
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
+                ),
+                const Divider(
+                  indent: 30,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      decimal ?? "",
+                      style: const TextStyle(
+                        color: Colors.blue,
+                        fontSize: 12.0,
+                      ),
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
+                ),
+                const Divider(
+                  indent: 30,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      binary ?? "",
+                      style: const TextStyle(
+                        color: Colors.yellow,
+                        fontSize: 12.0,
+                      ),
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
+                ),
+                const Gap(4),
+              ],
+            ),
+          );
   }
 }
